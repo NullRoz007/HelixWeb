de5e61a0647dbb2591c2b4d066698d8f8a3192d8
Ben Spencer <spencerbog1@gmail.com>
2025-12-01 01:34:53 +1300

    enabled ssr

--- app.config.js
+++ app.config.js
@@ -3,11 +3,6 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
     server: {
         baseURL: process.env.BASE_PATH,
-        static: true,
-        prerender: {
-        failOnError: true,
-        routes: ["/"],
-        crawlLinks: true,
-        }
+        ssr: true
     }
 });
