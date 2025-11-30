c546823a5f18d81edbf83b4a2b8c00b12ed152cf
Ben Spencer <spencerbog1@gmail.com>
2025-12-01 01:37:14 +1300

    enabled ssr

--- app.config.js
+++ app.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
+    start: {
+    ssr: true,
     server: {
-        baseURL: process.env.BASE_PATH,
-        ssr: true
+      baseURL: process.env.BASE_PATH,
+      preset: "static"
     }
+  }
 });
