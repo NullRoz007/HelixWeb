46b563b9d688e592cc6fd0ffac0e0a27cd1debdc
Ben Spencer <spencerbog1@gmail.com>
2025-12-01 01:32:06 +1300

    minor change to trigger rebuild

--- src/app.jsx
+++ src/app.jsx
@@ -8,9 +8,10 @@ import '@sakun/system.css';
 export default function App() {
   return (
     <Router
+      base={import.meta.env.SERVER_BASE_URL}
       root={props => (
         <MetaProvider>
-          <Title>Helix Asm - Web</Title>
+          <Title>Helix Asm - Web - v1.0</Title>
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
